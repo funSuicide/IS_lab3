d28934c0950c997302ea1e3be905e8665780aa90
Игорь Колядин <Digital-Corsair@yandex.com>
2021-11-22 14:14:48 +0400

    Добавил меню для приложения + прикрепил файл с исходным текстом.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 import pickle
 import io
+import argparse
 
 
 def generate_keys(path_symmetric_key: str, path_public_key: str, path_secret_key: str):
@@ -33,9 +34,6 @@ def generate_keys(path_symmetric_key: str, path_public_key: str, path_secret_key
         key_file.write(c_symmetric_key)
 
 
-generate_keys('symmetric_key.txt', 'public.pem', 'private.pem')
-
-
 def encryption_data(path_text: str, path_secret_key, path_symmetric_key: str, path_encrypted_text):
     with open(path_symmetric_key, mode='rb') as key_file:
         symmetric_key = key_file.read()
@@ -43,8 +41,8 @@ def encryption_data(path_text: str, path_secret_key, path_symmetric_key: str, pa
         private_bytes = pem_in.read()
         private_key = load_pem_private_key(private_bytes, password=None, )
     dc_key = private_key.decrypt(symmetric_key,
-                                  padd.OAEP(mgf=padd.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(),
-                                            label=None))
+                                 padd.OAEP(mgf=padd.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(),
+                                           label=None))
     f = open(path_text, 'r')
     data = f.read()
     f.close()
@@ -63,18 +61,15 @@ def encryption_data(path_text: str, path_secret_key, path_symmetric_key: str, pa
     f.close()
 
 
-encryption_data('text.txt', 'private.pem', 'symmetric_key.txt', 'c_text.txt')
-
-
-def decryption_data(path_secret_key, path_symmetric_key: str, path_encrypted_text, path_decrypted_text):
+def decryption_data(path_secret_key: str, path_symmetric_key: str, path_encrypted_text, path_decrypted_text):
     with open(path_symmetric_key, mode='rb') as key_file:
         symmetric_key = key_file.read()
     with open(path_secret_key, 'rb') as pem_in:
         private_bytes = pem_in.read()
         private_key = load_pem_private_key(private_bytes, password=None, )
     dc_key = private_key.decrypt(symmetric_key,
-                                  padd.OAEP(mgf=padd.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(),
-                                            label=None))
+                                 padd.OAEP(mgf=padd.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(),
+                                           label=None))
     f = open(path_encrypted_text, "rb")
     data = pickle.load(f)
     f.close()
@@ -86,8 +81,47 @@ def decryption_data(path_secret_key, path_symmetric_key: str, path_encrypted_tex
     unpadder = padding.ANSIX923(8).unpadder()
     unpadded_dc_data = unpadder.update(dc_data)
     dc_dataa = unpadded_dc_data.decode("UTF-8")
-    f = io.open("d_text.txt", mode="w", encoding="utf-8")
+    f = io.open(path_decrypted_text, mode="w", encoding="utf-8")
     f.write(str(dc_dataa))
 
 
-decryption_data('private.pem', 'symmetric_key.txt', 'c_text.txt', 'd_text.txt')
+parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('-gen', '--generation', help='Запускает режим генерации ключей')
+group.add_argument('-enc', '--encryption', help='Запускает режим шифрования')
+group.add_argument('-dec', '--decryption', help='Запускает режим дешифрования')
+
+args = parser.parse_args()
+if args.generation is not None:
+    print("Hello, user!^_^")
+    print("\nEnter the path for the public key: ")
+    public = input()
+    print("\nEnter the path for the private key: ")
+    private = input()
+    print("\nEnter the path for the symmetric key: ")
+    symmetric = input()
+    generate_keys(symmetric, public, private)
+    print("\nThe process is complete!")
+else:
+    if args.encryption is not None:
+        print("Enter the path to the source text: ")
+        text = input()
+        print("\nEnter the path for the private key: ")
+        private = input()
+        print("\nEnter the path for the symmetric key: ")
+        symmetric = input()
+        print("\nEnter the path to the ciphertext: ")
+        res = input()
+        encryption_data(text, private, symmetric, res)
+        print("\nThe process is complete!")
+    else:
+        print("Enter the path to the ciphertext: ")
+        text = input()
+        print("\nEnter the path for the private key: ")
+        private = input()
+        print("\nEnter the path for the symmetric key: ")
+        sym = input()
+        print("\nEnter the path for the decrypted text: ")
+        res = input()
+        decryption_data(private, sym, text, res)
+        print("\nThe process is complete!")
